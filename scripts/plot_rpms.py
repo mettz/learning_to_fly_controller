7013501f98cb37d75563fb236685b096b59fb7a0
Mattia Guazzaloca <mattia.guazzaloca@gmail.com>
2025-09-19 22:17:03 +0200

    added reset rpms version

--- scripts/plot_rpms.py
+++ scripts/plot_rpms.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 def extract_run_name(path: Path) -> str:
     stem = path.stem
@@ -24,13 +25,37 @@ def main():
 
     # ---------- plot ----------
     fig, ax = plt.subplots(figsize=(8,4), dpi=150)
+    plt.rcParams['lines.linewidth'] = 1.0
     ax.plot(t, m[:,0], label='m1')
     ax.plot(t, m[:,1], label='m2')
     ax.plot(t, m[:,2], label='m3')
     ax.plot(t, m[:,3], label='m4')
+    ax.set_xlim(5, 35)
+    ax.set_ylim(0, 70000)
     ax.set_xlabel('time [s]')
     ax.set_ylabel('motor RPM')
     ax.set_title(f"{run_name} – Motor RPMs vs Time")
+    ax.grid(True, alpha=0.3)
+    ax.legend()
+    fig.tight_layout()
+
+    # ---------- show ----------
+    plt.show()
+
+    window_size = 50  # number of samples in the sliding window
+    kernel = np.ones(window_size) / window_size
+
+    fig, ax = plt.subplots(figsize=(8,4), dpi=150)
+    plt.rcParams['lines.linewidth'] = 1.0
+    for i in range(4):
+        rolling_mean = np.convolve(m[:, i], kernel, mode='valid')
+        ax.plot(t[window_size-1:], rolling_mean, label=f'mean_m{i+1}')
+
+    ax.set_xlim(5, 35)
+    ax.set_ylim(0, 70000)
+    ax.set_xlabel('time [s]')
+    ax.set_ylabel('motor RPM')
+    ax.set_title(f"{run_name} – Motor RPMs vs Time (sliding window = {window_size})")
     ax.grid(True, alpha=0.3)
     ax.legend()
     fig.tight_layout()
