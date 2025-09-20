c17d508bbfa6b8d6d2694604151b672e32c2320c
Mattia Guazzaloca <mattia.guazzaloca@gmail.com>
2025-09-20 16:56:07 +0200

    added over nine hundred and 128 rollouts

--- rl_tools_adapter.cpp
+++ rl_tools_adapter.cpp
@@ -6,7 +6,7 @@
 #include <rl_tools/nn/layers/dense/operations_arm/dsp.h>
 #include <rl_tools/nn_models/sequential/operations_generic.h>
 
-#include "data/longer.h"
+#include "data/96_rollouts.h"
 
 #define RL_TOOLS_CONTROL_STATE_ROTATION_MATRIX
 #define RL_TOOLS_DISABLE_TEST
